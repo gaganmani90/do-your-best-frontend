d1c5c67bb079f4c3414317bce2845f85dd226fd1
Gagan Mani <gaganmani90@gmail.com>
2021-12-05 19:23:33 -0800

    Add welcome guest user

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import PerformanceForm from "./PerformanceForm";
 
 const Home = () => {
     return (
-        <><h1>How did you do today?</h1>
+        <><h3>Welcome <b>guest user</b>, how did you do today?</h3>
             <PerformanceForm />
         </>
     );
