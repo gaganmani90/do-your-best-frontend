b7355b74cbdbb44748c5ec4dd6e2c67f90e44755
Gagan Mani <gaganmani90@gmail.com>
2021-12-22 12:55:57 -0800

    Add ESLint

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import './App.css';
-import React from 'react';
-import DYBNavigation from './DYBNavigation';
-import { Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import { Badge, Container } from "react-bootstrap";
-import Signup from './components/signup';
-import Login from './components/login';
-import PrivateRoute from './components/privateRoute';
-import ForgetPassword from './components/forgetpassword';
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+import './App.css'
+import DYBNavigation from './DYBNavigation'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Home'
+import { Container } from 'react-bootstrap'
+import Signup from './components/signup'
+import Login from './components/login'
+import PrivateRoute from './components/privateRoute'
+import ForgetPassword from './components/forgetpassword'
 
-function App() {
+function App () {
   return (
-    <Container>
-      <div className="App">
-        <DYBNavigation />
-        <Routes>
-          <Route path='/' element={<PrivateRoute />} >
-            <Route path='/' element={<Home />} />
-          </Route>
-          <Route exact path="/Signup" element={<Signup />} />
-          <Route exact path="/Login" element={<Login />} />
-          <Route exact path="/forget-password" element={<ForgetPassword />} />
-        </Routes>
-      </div>
-    </Container>
-  );
+        <Container>
+            <div className="App">
+                <DYBNavigation/>
+                <Routes>
+                    <Route path='/' element={<PrivateRoute/>}>
+                        <Route path='/' element={<Home/>}/>
+                    </Route>
+                    <Route exact path="/Signup" element={<Signup/>}/>
+                    <Route exact path="/Login" element={<Login/>}/>
+                    <Route exact path="/forget-password" element={<ForgetPassword/>}/>
+                </Routes>
+            </div>
+        </Container>
+  )
 }
 
-export default App;
+export default App
